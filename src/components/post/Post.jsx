8b94d36fa646355166b7d0e5c3fdb5ac899a02fb
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2023-01-22 23:53:32 +0200

    Added Profile page

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,17 @@
 import { MoreVert } from "@mui/icons-material";
 import "./post.css";
 import { Users } from "../../dummyData";
+import { useState } from "react";
 
 export default function Post({ post }) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -30,9 +39,19 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="/assets/like.png" alt="" />
-            <img className="likeIcon" src="/assets/heart.png" alt="" />
-            <span className="postLikeCouter">{post.like} people like it</span>
+            <img
+              className="likeIcon"
+              src="/assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src="/assets/heart.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="postLikeCouter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
